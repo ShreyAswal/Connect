6ebb241ff15ec682b0baa334741d6a6927498faf
ShreyAswal <aswalshrey@gmail.com>
2024-04-05 23:09:17 +0530

    parsing middleware added

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+
+import AuthRoute from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
 const app = express();
 
+// PORT has to access the port number only after config() is called.
 dotenv.config();
 const PORT = process.env.PORT;
 
+//Middleware
+app.use(express.json()); //Middleware to parse JSON data
 
-//Root Route
-app.get('/',(req,res) => {
-    res.send('Hello World!!!');
-})
+app.use("/api/auth", AuthRoute);
 
-
-app.listen(PORT,() => {
-    console.log('Server is running on port ',PORT);
-})
+app.listen(PORT, () => {
+    connectToMongoDB();
+  console.log("Server is running on port ", PORT);
+});
