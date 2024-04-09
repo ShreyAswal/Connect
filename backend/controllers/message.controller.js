1c5ef42fcd2befb7b928b99eadd410a7141f7f07
ShreyAswal <aswalshrey@gmail.com>
2024-04-09 23:33:38 +0530

    Able to get messages from DB between sender & receiver!

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js"
-const sendMessage = async (req, res) => {
+
+export const sendMessage = async (req, res) => {
   try {
     console.log("Request body: ", req.body);
     const message = req.body.message;
@@ -35,9 +36,17 @@ const sendMessage = async (req, res) => {
 
     console.log("Conversation: ", conversation);
 
-    // Save the conversation and message
-    await conversation.save();
-    await newMessage.save();
+
+    // SOCKET.IO functionality goes here
+
+    // Save the conversation and message - 2 methods.
+
+    // With this format the conversation will be saved first and then the message after syncronously
+    // await conversation.save();
+    // await newMessage.save();
+
+    // This let's both the operations to be done in parallel and wait for both to complete
+    await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
 
@@ -48,4 +57,28 @@ const sendMessage = async (req, res) => {
   }
 };
 
-export default sendMessage;
+export const getMessage = async (req, res) => {
+  try {
+    
+    const { id: userToChatId } = req.params; // the id in the url is of the user the logged in user is chatting to.
+    const senderId = req.user._id; // Getting the senderId from the authenticated user in the request object, the user who is logged in
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [ senderId, userToChatId]} // Fetches All messages of loggenIn User and userId in the url
+    }).populate("messages"); // Not reference to messages, but the actual messages using populate
+
+    // If conversation doesn't exist, return an empty array
+    if(!conversation){
+      return res.status(200).json([]);
+    }
+    
+    // Get the messages from the conversation
+    const messages = conversation.messages;
+    res.status(200).json(messages);
+
+
+  } catch (error) {
+    console.log("Error in getting message: ", error.message);
+    res.status(500).json({ message: "Internal server error!" });
+  }
+};
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import sendMessage from '../controllers/message.controller.js';
+import { sendMessage , getMessage } from '../controllers/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 // We are using protectRoute middleware to authenticate the user before sending the message
 // We don't send any response in protectedRoute middleware, so with next() we move to the next function sendMessage
+router.get('/:id', protectRoute, getMessage);
 router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
