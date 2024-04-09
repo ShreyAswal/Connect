5d2e5d289e23c25cdd09d0d4c0b469bc81d7e9c8
ShreyAswal <aswalshrey@gmail.com>
2024-04-09 23:30:20 +0530

    Added route middleware for users & comments

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import AuthRoute from "./routes/auth.routes.js";
-import MessageRoute from "./routes/message.routes.js"
+import AuthRoutes from "./routes/auth.routes.js";
+import MessageRoutes from "./routes/message.routes.js"
+import UserRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -15,10 +16,11 @@ const PORT = process.env.PORT;
 app.use(express.json()); //Middleware to parse JSON data
 app.use(cookieParser()); //Middleware to parse cookies
 
-app.use("/api/auth", AuthRoute);
-app.use("/api/messages", MessageRoute);
+app.use("/api/auth", AuthRoutes); //Middleware to use the routes for authentication
+app.use("/api/messages", MessageRoutes); //Routes for messages
+app.use("/api/users", UserRoutes); //Routes for users
 
 app.listen(PORT, () => {
-    connectToMongoDB();
+  connectToMongoDB();
   console.log("Server is running on port ", PORT);
 });
