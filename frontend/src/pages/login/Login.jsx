843fbd125977573c1158499ac39fd7e369dc496c
ShreyAswal <aswalshrey@gmail.com>
2024-04-11 23:44:17 +0530

    folder structure setup

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,12 @@
+
+const Login = () => {
+  return (
+    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+        <div className='w-full p-6 bg-purple-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-300'>
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>Login!</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Login;
