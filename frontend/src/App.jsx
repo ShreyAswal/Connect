017bbfc9dbf22923dcb8f067fbc3535fc3cefcb3
ShreyAswal <aswalshrey@gmail.com>
2024-04-13 00:42:17 +0530

    Tailwind UI Setup & react-icons used

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.css";
-import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className='p-16 h-screen flex justify-center items-center'>
-      <Login/>
-    
+      <Home/>
     </div>
   );
 }
