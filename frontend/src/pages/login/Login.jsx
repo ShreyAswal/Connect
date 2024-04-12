27bfc4dd6359fb05e8cbf8eb91882b7b1c86f4a5
ShreyAswal <aswalshrey@gmail.com>
2024-04-13 00:36:25 +0530

    Login & Signup UI designed

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,12 +1,49 @@
-
 const Login = () => {
   return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-        <div className='w-full p-6 bg-purple-600 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-300'>
-        <h1 className='text-3xl font-semibold text-center text-gray-300'>Login!</h1>
-        </div>
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+      <div className="w-full p-6 rounded-lg shadow-md bg-purple-600 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-pink-400">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login
+          <span className="text-pink-500"> To Connect :)</span>
+        </h1>
+
+        <form>
+          <div>
+            <lable className="label p-2">
+              <span className="text-base label-text">Username</span>
+            </lable>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="input input-bordered input-secondary max-w-xs w-full h-10"
+            />
+          </div>
+
+          <div>
+            <lable className="label p-2">
+              <span className="text-base label-text">Password</span>
+            </lable>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="input input-bordered input-secondary max-w-xs w-full h-10"
+            />
+          </div>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-pink-600 mt-2 inline-block"
+          >
+            {"Don't"} have an account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-sm mt-2">Login</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
