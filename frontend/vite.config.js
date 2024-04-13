7df1d981ec770bc34716e63d2c127fd618c9e79b
ShreyAswal <aswalshrey@gmail.com>
2024-04-14 01:07:25 +0530

    React prop type disabled & set target url prefix for development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        // The backend server address prefix for any request sent from the frontend server to the backend server
+        target: "http://localhost:5000",
+      },
+    },
+  },
+});
