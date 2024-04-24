bd8820e848a91edbff868bbd469f1b4d51514735
ShreyAswal <aswalshrey@gmail.com>
2024-04-24 18:59:55 +0530

    Getting users to chat with & additional updates

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,23 +1,36 @@
-const Conversation = () => {
+import useConversation from "../../zustand/useConversation";
+
+const Conversation = ({ conversation, lastIdx, emoji }) => {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  const isSelected = selectedConversation?._id === conversation._id;
+
   return (
     <>
-      <div className="flex gap-2 items-center hover:border-y-pink-500 rounded p-2 py-1 cursor-pointer">
+      <div
+        className={`flex gap-2 items-center hover:border-y-pink-500 rounded p-2 py-1 cursor-pointer ${
+          isSelected ? "bg-blue-400" : ""
+        }`}
+        onClick={() => setSelectedConversation(conversation)}
+      >
         <div className="avatar online ">
-            <div className="w-12 rounded-full">
-                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"></img>
-            </div>
+          <div className="w-12 rounded-full">
+            <img src={conversation.profilePic} alt="user avatar"></img>
+          </div>
         </div>
 
         <div className="flex flex-col flex-1">
-            <div className="flex gap-3 justify-between">
-                <p className="font-bold text-gray-200">Boa Hanckock</p>
-                <span className="text-xl">🙂</span>
-            </div>
+          <div className="flex gap-3 justify-between">
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
+          </div>
         </div>
       </div>
-      <div className='divider my-0 py-0 h-1'/>
-    </>
-  )
-}
+      {/* <div className='divider my-0 py-0 h-1'/> */}
 
-export default Conversation
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
+    </>
+  );
+};
+
+export default Conversation;
