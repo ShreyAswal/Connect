f85788f4cabff662edbcf88d0e16a15056d66c8b
ShreyAswal <aswalshrey@gmail.com>
2024-04-27 21:59:19 +0530

    Search User feature implemented

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
 import { MdPersonSearch } from "react-icons/md";
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import toast from "react-hot-toast";
 
 const SearchInput = () => {
+  const [search, setSearch] = useState("");
+  const { setSelectedConversation } = useConversation();
+  const { conversations } = useGetConversations();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+    if (search.length < 3) {
+      return toast.error("Search term must be at least 3 letters long!");
+    }
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No user found with that name!");
+  };
   return (
-    <form className="flex items-center gap-2">
+    <form className="flex items-center gap-2" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
       />
       <button type="submit" className="btn btn-circle bg-pink-500 text-white">
         {/* Imported from react icons for search button*/}
-        <MdPersonSearch className="w-6 h-6 outline-none"/>
+        <MdPersonSearch className="w-6 h-6 outline-none" />
       </button>
     </form>
   );
-}
+};
 
 export default SearchInput;
