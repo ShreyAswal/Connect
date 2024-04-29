3968ac1fd89340f2bda2cb6a9071267b0759b43d
ShreyAswal <aswalshrey@gmail.com>
2024-04-29 23:04:29 +0530

    Backend logic to show online status in realtime

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import AuthRoutes from "./routes/auth.routes.js";
 import MessageRoutes from "./routes/message.routes.js"
 import UserRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import { app } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 // PORT has to access the port number only after config() is called.
 dotenv.config();
@@ -20,7 +20,7 @@ app.use("/api/auth", AuthRoutes); // Routes for authentication
 app.use("/api/messages", MessageRoutes); //Routes for messages
 app.use("/api/users", UserRoutes); //Routes for users
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log("Server is running on port ", PORT);
 });
